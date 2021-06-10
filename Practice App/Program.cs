e8cf631531a0f47fa8e23b5fd7b402960fe74da9
dougl <dougl@GOD-OF-WAR>
2021-06-09 19:59:24 -0500

    Add project files.

---
+++ Practice App/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Practice_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(maths(input,10, 5));
+        }
+        static int maths(string op, int numberone, int numbertwo)
+        {
+            if (op == "add")
+            {
+                return numberone + numbertwo;
+            }
+            else if (op == "subtract")
+            {
+                return numberone - numbertwo;
+            }
+            else if (op == "multiply")
+            {
+                return numberone * numbertwo;
+            }
+            return 1;
+        }
+    }
+}
